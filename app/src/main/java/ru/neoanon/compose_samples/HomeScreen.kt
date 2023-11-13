c53c45f60286d429b172a07612f371bcc7d9fd1e
shtefan <e.shtefan@cft.ru>
2023-11-13 11:03:25 +0700

    Базовые лейауты и строковые ресурсы

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -2,12 +2,23 @@ package ru.neoanon.compose_samples
 
 import android.graphics.Paint.Align
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,16 +27,21 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen() {
-	Text(
-		text = "Home screen title",
-		fontSize = 16.sp,
-		color = Color.White,
-		maxLines = 1,
-		overflow = TextOverflow.Ellipsis,
-		textAlign = TextAlign.Center,
-		modifier = Modifier.background(color = Color.Black)
-			.fillMaxWidth(),
-	)
+	Box(modifier = Modifier.fillMaxSize()){
+		Text(
+			text = stringResource(id = R.string.title),
+			fontSize = 32.sp,
+			maxLines = 1,
+			overflow = TextOverflow.Ellipsis,
+		)
+		Text(
+			text = "Description",
+			fontSize = 16.sp,
+			maxLines = 1,
+			overflow = TextOverflow.Ellipsis,
+			modifier = Modifier.align(BottomCenter)
+		)
+	}
 }
 
 @Preview(showBackground = true)
