9334227455c9d65dc41f3f972989c64f040a0a86
shtefan <e.shtefan@cft.ru>
2023-11-14 11:16:17 +0700

    Background и border. Изображения.

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -1,46 +1,42 @@
 package ru.neoanon.compose_samples
 
-import android.graphics.Paint.Align
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.BottomCenter
-import androidx.compose.ui.Alignment.Companion.CenterVertically
-import androidx.compose.ui.Alignment.Companion.Start
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 
 @Composable
 fun HomeScreen() {
-	Box(modifier = Modifier.fillMaxSize()){
-		Text(
-			text = stringResource(id = R.string.title),
-			fontSize = 32.sp,
-			maxLines = 1,
-			overflow = TextOverflow.Ellipsis,
-		)
-		Text(
-			text = "Description",
-			fontSize = 16.sp,
-			maxLines = 1,
-			overflow = TextOverflow.Ellipsis,
-			modifier = Modifier.align(BottomCenter)
-		)
+	Box(contentAlignment = Center, modifier = Modifier.fillMaxSize()) {
+		Box(
+			contentAlignment = Center,
+			modifier = Modifier
+				.width(160.dp)
+				.height(140.dp)
+				.border(
+					width = 2.dp,
+					shape = RoundedCornerShape(8.dp),
+					brush = Brush.linearGradient(colors = listOf(Color.Yellow, Color.Blue, Color.Green)),
+				)
+		) {
+			//Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = null)
+			AsyncImage(
+				model = "https://developer.android.com/images/android-go/next-billion-users_856.png",
+				contentDescription = null
+			)
+		}
 	}
 }
 
