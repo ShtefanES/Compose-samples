fa0b5c60b1c0c0215658b07f8ebcf7bd2f0c3239
shtefan <e.shtefan@cft.ru>
2023-11-15 11:14:27 +0700

    State

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -1,47 +1,12 @@
 package ru.neoanon.compose_samples
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.clickable
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 
 @Composable
-fun HomeScreen() {
-	Box(contentAlignment = Center, modifier = Modifier.fillMaxSize()) {
-		Box(
-			contentAlignment = Center,
-			modifier = Modifier
-				.width(160.dp)
-				.height(140.dp)
-				.border(
-					width = 2.dp,
-					shape = RoundedCornerShape(8.dp),
-					brush = Brush.linearGradient(colors = listOf(Color.Yellow, Color.Blue, Color.Green)),
-				)
-		) {
-			//Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = null)
-			AsyncImage(
-				model = "https://developer.android.com/images/android-go/next-billion-users_856.png",
-				contentDescription = null
-			)
-		}
-	}
-}
-
-@Preview(showBackground = true)
-@Composable
-fun HomeScreenPreview() {
-	HomeScreen()
+fun HomeScreen(state: State<Int>, onCounterClick: () -> Unit) {
+	Text(text = "Clicks: ${state.value}", modifier = Modifier.clickable(onClick = onCounterClick))
 }
--- app/src/main/java/ru/neoanon/compose_samples/MainActivity.kt
+++ app/src/main/java/ru/neoanon/compose_samples/MainActivity.kt
@@ -3,13 +3,17 @@ package ru.neoanon.compose_samples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.mutableStateOf
 
 class MainActivity : ComponentActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		val counter = mutableStateOf(0)
 		setContent {
-			HomeScreen()
+			HomeScreen(counter) {
+				counter.value++
+			}
 		}
 	}
 }
