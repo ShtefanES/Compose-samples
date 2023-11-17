05db035035af0eb1a43aa73007a38aa55e17e13e
shtefan <e.shtefan@cft.ru>
2023-11-17 11:26:56 +0700

    Список, call site, key

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -1,30 +1,48 @@
 package ru.neoanon.compose_samples
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Checkbox
+import android.util.Log
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.CenterVertically
-import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 const val TAG = "MY_TAG"
 
 @Composable
 fun HomeScreen() {
-	var checked by remember { mutableStateOf(false) }
-	Column {
-		Row(verticalAlignment = CenterVertically) {
-			Checkbox(checked = checked, onCheckedChange = { value -> checked = value })
-			Text("More details", fontSize = 18.sp)
-		}
-		if (checked) {
-			Text(text = stringResource(id = R.string.details))
+	Log.d(TAG, "HomeScreen")
+	val list = remember {
+		List(20) { "Item ${it + 1}" }.toMutableStateList()
+	}
+	LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp),
+			   contentPadding = PaddingValues(16.dp),
+			   modifier = Modifier
+					  .border(width = 2.dp, color = Color.Green)) {
+		items(list) { value ->
+			SomeItem(value)
 		}
 	}
+}
+
+@Composable
+fun SomeItem(text: String) {
+	Log.d(TAG, "SomeItem $text")
+	Text(
+		text = text, fontSize = 20.sp,
+		modifier = Modifier
+			.fillMaxWidth()
+			.border(width = 1.dp, color = Color.Black)
+			.padding(16.dp)
+	)
 }
