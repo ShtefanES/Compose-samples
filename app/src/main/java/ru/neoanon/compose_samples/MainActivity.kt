ba2d2b77e611290a122b8fb8190cecba57936930
shtefan <e.shtefan@cft.ru>
2023-12-13 09:58:22 +0700

    Navigation component

--- app/src/main/java/ru/neoanon/compose_samples/MainActivity.kt
+++ app/src/main/java/ru/neoanon/compose_samples/MainActivity.kt
@@ -5,49 +5,45 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Column {
-				var route by remember { mutableStateOf("home") }
-				Box(modifier = Modifier.weight(1f).padding(16.dp)) {
-					when (route) {
-						"home" -> HomeScreen(
-							onNavigateToOrders = {route = "orders"},
-							onNavigateToUsers = {route = "users"},
+				Column(modifier = Modifier.fillMaxSize()) {
+					val navController = rememberNavController()
+					NavHost(navController = navController, startDestination = "home", modifier = Modifier.weight(1f).padding(16.dp)) {
+						composable("home") { HomeScreen(
+							onNavigateToUsers = { navController.navigate("users")  },
+							onNavigateToOrders = { navController.navigate("orders") },
 						)
-
-						"orders" -> OrdersScreen()
-
-						"users" -> UsersScreen()
+						}
+						composable("orders") { OrdersScreen() }
+						composable("users") { UsersScreen() }
+					}
+					Row(
+						modifier = Modifier
+							.fillMaxWidth()
+							.padding(16.dp),
+						horizontalArrangement = Arrangement.SpaceEvenly
+					) {
+						Text(text = "Home", modifier = Modifier.clickable { navController.navigate("home") })
+						Text(text = "Orders", modifier = Modifier.clickable { navController.navigate("orders") })
+						Text(text = "Users", modifier = Modifier.clickable { navController.navigate("users") })
 					}
 				}
-				Row(
-					modifier = Modifier.fillMaxWidth().padding(16.dp),
-					horizontalArrangement = Arrangement.SpaceEvenly
-				) {
-					Text(text = "Home", modifier = Modifier.clickable { route = "home"})
-					Text(text = "Orders", modifier = Modifier.clickable { route = "orders" })
-					Text(text = "Users", modifier = Modifier.clickable { route = "users" })
-				}
 			}
-		}
 	}
 }
