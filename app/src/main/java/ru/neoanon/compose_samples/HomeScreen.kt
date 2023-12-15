15c5e1610e268186a7377fff5dc81f88fe451c63
shtefan <e.shtefan@cft.ru>
2023-12-15 11:30:30 +0700

    Composable life cycle and RememberObserver

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -1,5 +1,6 @@
 package ru.neoanon.compose_samples
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -10,46 +11,62 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.RememberObserver
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
+import okhttp3.internal.toHexString
 
 const val TAG = "MY_TAG"
 
 @Composable
-fun HomeScreen(
-	onNavigateToOrders: () -> Unit,
-	onNavigateToUsers: () -> Unit,
-) {
-	Column() {
-		Text(text = "Home screen")
-
-		Text(text = "go to Orders", Modifier.clickable(onClick = onNavigateToOrders))
-
-		Text(text = "go to Users", Modifier.clickable(onClick = onNavigateToUsers))
-	}
-}
-
-@Composable
-fun OrdersScreen() {
-	Text(text = "Orders screen")
-}
-
-@Composable
-fun UsersScreen() {
-	Text(text = "Users screen")
-}
-
-@Composable
-fun UserScreen(id: String?) {
+fun HomeScreen() {
 	Column {
-		Text(text = "User screen")
-
-		Text(text = "User $id")
+		var checked by remember { mutableStateOf(false) }
+		Checkbox(checked = checked, onCheckedChange = { checked = it })
+		if (checked) {
+			val myObject = remember { MyObject() }
+		}
 	}
+}
+
+@Composable
+fun ClickCounter() {
+	var count by remember { mutableStateOf(0) }
+	Text(
+		text = "Count $count",
+		modifier = Modifier.clickable { count++ }
+	)
+}
+
+class MyObject : RememberObserver {
+
+	init {
+		Log.d(TAG, "init")
+	}
+
+	override fun onRemembered() {
+		Log.d(TAG, "onRemembered")
+	}
+
+	override fun onForgotten() {
+		Log.d(TAG, "onForgotten")
+	}
+
+	override fun onAbandoned() {
+		Log.d(TAG, "onAbandoned")
+	}
+
 }
--- app/src/main/java/ru/neoanon/compose_samples/MainActivity.kt
+++ app/src/main/java/ru/neoanon/compose_samples/MainActivity.kt
@@ -5,14 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -24,36 +30,7 @@ class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-				Column(modifier = Modifier.fillMaxSize()) {
-					val navController = rememberNavController()
-					NavHost(navController = navController, startDestination = "home", modifier = Modifier.weight(1f).padding(16.dp)) {
-						composable("home") { HomeScreen(
-							onNavigateToUsers = { navController.navigate("users")  },
-							onNavigateToOrders = { navController.navigate("orders") },
-						)
-						}
-						composable("orders") { OrdersScreen() }
-						composable("users") { UsersScreen() }
-						composable(
-							route = "user/{id}",
-							arguments = listOf(navArgument("id") { type = NavType.StringType }),
-						) { navBackStackEntity ->
-							val userId = navBackStackEntity.arguments?.getString("id")
-							UserScreen(userId)
-						}
-					}
-					Row(
-						modifier = Modifier
-							.fillMaxWidth()
-							.padding(16.dp),
-						horizontalArrangement = Arrangement.SpaceEvenly
-					) {
-						Text(text = "Home", modifier = Modifier.clickable { navController.navigate("home") })
-						Text(text = "Orders", modifier = Modifier.clickable { navController.navigate("orders") })
-						Text(text = "Users", modifier = Modifier.clickable { navController.navigate("users") })
-						Text(text = "User", modifier = Modifier.clickable { navController.navigate("user/100500") })
-					}
-				}
+			HomeScreen()
 			}
 	}
 }
