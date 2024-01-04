464346dddaa3da89340f972ee14374fdb97a6a5b
shtefan <e.shtefan@cft.ru>
2024-01-04 19:44:47 +0700

    derivedStateOf

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -4,8 +4,11 @@ import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Checkbox
+import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
@@ -15,17 +18,31 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 const val TAG = "MY_TAG"
 
 @Composable
-fun HomeScreen() {
-	val count by produceState(initialValue = 0) {
-		while (true) {
-			delay(1000)
-			value++
-		}
-	}
+fun HomeScreen(
+) {
+	Column {
+		val sliderPosition = remember { mutableStateOf(1f) }
+		MySlider(sliderPosition = sliderPosition)
 
-	Text(text = "count = $count")
+		val roundedPosition by remember {
+			derivedStateOf { sliderPosition.value.roundToInt() }
+		}
+		Text(text = "$roundedPosition")
+		Log.d(TAG, "HomeScreen $roundedPosition")
+	}
+}
+
+@Composable
+fun MySlider(sliderPosition: MutableState<Float>) {
+	Log.d(TAG, "MySlider")
+	Slider(
+		value = sliderPosition.value,
+		valueRange = 1f..10f,
+		onValueChange = { sliderPosition.value = it }
+	)
 }
