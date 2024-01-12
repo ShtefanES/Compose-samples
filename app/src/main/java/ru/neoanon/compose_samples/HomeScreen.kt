81e3c0d8353f052c16fa18d20c7dada84e4b1d15
shtefan <e.shtefan@cft.ru>
2024-01-12 10:59:11 +0700

    Theme

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -1,16 +1,9 @@
 package ru.neoanon.compose_samples
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Checkbox
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontStyle
 
 const val TAG = "MY_TAG"
@@ -18,35 +11,7 @@ val LocalFontStyle = compositionLocalOf { FontStyle.Normal }
 
 @Composable
 fun HomeScreen() {
-	Column {
-		val italicState = remember { mutableStateOf(false) }
-		MyCheckbox("Italic", italicState)
-
-		val fontStyle = if (italicState.value) FontStyle.Italic else FontStyle.Normal
-		CompositionLocalProvider(LocalFontStyle provides fontStyle) {
-			MyText(text = "Text 1")
-			MyText(text = "Text 2")
-			MyText(text = "Text 3")
-			MyText(text = "Text 4")
-		}
-		MyText(text = "Text 5")
+	Button(onClick = { }) {
+		Text(text = "Some text")
 	}
-}
-
-@Composable
-fun MyCheckbox(
-	text: String,
-	checked: MutableState<Boolean>,
-) {
-	Row(verticalAlignment = Alignment.CenterVertically) {
-		Checkbox(checked = checked.value, onCheckedChange = { checked.value = it })
-		Text(text = text)
-	}
-}
-
-@Composable
-fun MyText(
-	text: String
-) {
-	Text(text = text, fontStyle = LocalFontStyle.current)
 }
--- app/src/main/java/ru/neoanon/compose_samples/MainActivity.kt
+++ app/src/main/java/ru/neoanon/compose_samples/MainActivity.kt
@@ -11,12 +11,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
@@ -24,13 +27,16 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import ru.neoanon.compose_samples.ui.theme.ComposesamplesTheme
 
 class MainActivity : ComponentActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			HomeScreen()
+			ComposesamplesTheme() {
+				HomeScreen()
 			}
+		}
 	}
 }
