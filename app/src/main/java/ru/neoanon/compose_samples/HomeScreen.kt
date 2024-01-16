2d8f9ae970e6a35b9593914d067e5fd41b50f6b0
shtefan <e.shtefan@cft.ru>
2024-01-16 10:39:21 +0700

    LayoutModifier

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -1,5 +1,7 @@
 package ru.neoanon.compose_samples
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,6 +18,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 
@@ -24,37 +28,18 @@ val LocalFontStyle = compositionLocalOf { FontStyle.Normal }
 
 @Composable
 fun HomeScreen() {
-	Scaffold(
-		topBar = {
-			TopAppBar {
-				Text("Top bar")
-			}
-		},
-		bottomBar = {
-			BottomAppBar {
-				Text("Bottom bar")
-			}
-		},
-		floatingActionButton = {
-			FloatingActionButton(onClick = {  }) {
-				Icon(
-					imageVector = Icons.Filled.Add,
-					contentDescription = null
-				)
-			}
-		},
-		drawerContent = {
-			Text("Drawer item 1", modifier = Modifier.padding(8.dp))
-			Text("Drawer item 2", modifier = Modifier.padding(8.dp))
-			Text("Drawer item 3", modifier = Modifier.padding(8.dp))
-		},
-		content = {paddingValues ->
-			Box(
-				modifier = Modifier.fillMaxSize().padding(paddingValues),
-				contentAlignment = Alignment.Center
-			) {
-				Text("Content")
-			}
-		}
-	)
+	Box(
+		modifier = Modifier.background(Color.LightGray)
+	) {
+		Text(text = "Some text", modifier = Modifier.myLayout())
+	}
+}
+
+fun Modifier.myLayout() = layout { measurable, constraints ->
+	Log.d(TAG, "constraints = $constraints")
+	val placeable = measurable.measure(constraints) // Measure children
+	Log.d(TAG, "placeable, width = ${placeable.width}, height = ${placeable.height} ")
+	layout(placeable.width, placeable.height) { // Decide own size
+		placeable.placeRelative(0, 0) // Place children
+	}
 }
