101d01a77ce249a5c510bcb3d815302f59279faf
shtefan <e.shtefan@cft.ru>
2024-02-19 20:33:03 +0700

    Custom Layout - Timeline. Parent Data

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -1,60 +1,68 @@
 package ru.neoanon.compose_samples
 
-import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Button
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.ParentDataModifier
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.Density
 
 const val TAG = "MY_TAG"
 val LocalFontStyle = compositionLocalOf { FontStyle.Normal }
 
 @Composable
 fun HomeScreen() {
-	MyColumn(modifier = Modifier.background(Color.Gray)) {
-		Text("Text 1")
-		Text("Text 2")
-		Text("Text 3")
-		Text("Text 4")
+	Timeline {
+		Text("Task 1")
+		Text("Task 2", modifier = Modifier.position(Position.PARALLEL))
+		Text("Task 3")
+		Text("Task 4")
+		Text("Task 5", modifier = Modifier.position(Position.END))
+
+		Text("Task 6")
+		Text("Task 7", modifier = Modifier.position(Position.PARALLEL))
+		Text("Task 8", modifier = Modifier.position(Position.PARALLEL))
+		Text("Task 9")
+		Text("Task 10")
 	}
 }
 
 @Composable
-fun MyColumn(
-	modifier: Modifier = Modifier,
-	content: @Composable () -> Unit
+fun Timeline(
+	modifier: Modifier = Modifier, content: @Composable () -> Unit
 ) {
 	Layout(
-		modifier = modifier,
-		content = content
+		modifier = modifier, content = content
 	) { measurables, constraints ->
-		val placeables = measurables.map {measurable ->
+		val placeables = measurables.map { measurable ->
 			measurable.measure(constraints) // Measure children
 		}
-		layout(placeables.maxOf { it.width }, placeables.sumOf { it.height }) { // Decide own size
+		layout(constraints.maxWidth, constraints.maxHeight) { // Decide own size
+			var x = 0
 			var y = 0
 			placeables.forEach { placeable ->
-				placeable.placeRelative(x = 0, y = y) // Place children
+				val position = ((placeable as? Measurable)?.parentData as? PositionParentData)?.position
+				placeable.placeRelative(x = x, y = y) // Place children
+				when (position) {
+					Position.PARALLEL -> {}
+
+					Position.END      -> x = 0
+					null              -> x += placeable.width
+				}
 				y += placeable.height
 			}
 		}
 	}
 }
+
+class PositionParentData(
+	val position: Position
+) : ParentDataModifier {
+
+	override fun Density.modifyParentData(parentData: Any?): Any? = this@PositionParentData
+}
+
+fun Modifier.position(position: Position) = this.then(PositionParentData(position))
---
+++ app/src/main/java/ru/neoanon/compose_samples/Position.kt
@@ -0,0 +1,5 @@
+package ru.neoanon.compose_samples
+
+enum class Position {
+	END, PARALLEL
+}
