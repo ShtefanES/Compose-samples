c8f6871c86b3505900ceba82fa9c8dc165f45f4c
shtefan <e.shtefan@cft.ru>
2024-03-27 18:52:02 +0700

    TargetBasedAnimation

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -1,41 +1,45 @@
 package ru.neoanon.compose_samples
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.TargetBasedAnimation
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.tween
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.produceState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.text.font.FontStyle
-import kotlinx.coroutines.delay
+import androidx.compose.ui.unit.sp
 
 const val TAG = "MY_TAG"
 val LocalFontStyle = compositionLocalOf { FontStyle.Normal }
 
 @Composable
 fun HomeScreen() {
-	val degrees by produceState(initialValue = 0f) {
-		while (true) {
-			delay(100)
-			value = (value + 5f).rem(360)
-		}
+	val animation = remember {
+		TargetBasedAnimation(
+			animationSpec = tween(5000, easing = LinearEasing),
+			typeConverter = Int.VectorConverter,
+			initialValue = 0,
+			targetValue = 100
+		)
 	}
 
-	Spacer(modifier = Modifier
-		.fillMaxSize()
-		.graphicsLayer { rotationZ = degrees }
-		.drawBehind {
-			drawRect(
-				brush = Brush.linearGradient(listOf(Color.Green, Color.Blue)),
-				size = size.div(2f),
-				topLeft = Offset(size.width / 4f, size.height / 4f)
-			)
+	val animationValueState = remember {
+		mutableStateOf(0)
+	}
+
+	Text("value = ${animationValueState.value}", fontSize = 30.sp)
+
+	LaunchedEffect(key1 = Unit) {
+		val startTime = withFrameNanos { it }
+		var playTime = 0L
+		while(playTime <= animation.durationNanos) {
+			playTime = withFrameNanos { it } - startTime
+			animationValueState.value = animation.getValueFromNanos(playTime)
 		}
-	)
+	}
 }
