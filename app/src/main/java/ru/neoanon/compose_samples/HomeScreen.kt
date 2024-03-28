c99c5a3b308c30b02a13ed286fcfe3a9cda8f3dc
shtefan <e.shtefan@cft.ru>
2024-03-28 19:19:18 +0700

    Animatable custom Easing

--- app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
+++ app/src/main/java/ru/neoanon/compose_samples/HomeScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.Easing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.TargetBasedAnimation
 import androidx.compose.animation.core.VectorConverter
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -27,6 +29,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 const val TAG = "MY_TAG"
@@ -34,37 +37,36 @@ val LocalFontStyle = compositionLocalOf { FontStyle.Normal }
 
 @Composable
 fun HomeScreen() {
-	val animatable = remember {
+	val myAnimation = remember {
 		Animatable(initialValue = 10.dp, typeConverter = Dp.VectorConverter)
 	}
 
-	Column {
-		Row {
-			val scope = rememberCoroutineScope()
-			MyTargetButton(animatable, targetValue = 10.dp, duration = 5000)
-			Spacer(modifier = Modifier.width(16.dp))
-			MyTargetButton(animatable, targetValue = 300.dp, duration = 1000)
-		}
-		Spacer(
-			modifier = Modifier
-				.background(Color.Green)
-				.height(50.dp)
-				.width(animatable.value)
+	Spacer(modifier = Modifier
+		.background(Color.Green)
+		.height(50.dp)
+		.width(myAnimation.value)
+	)
+
+	LaunchedEffect(Unit) {
+		delay(1000)
+		myAnimation.animateTo(
+			targetValue = 250.dp,
+			animationSpec = createSpec()
 		)
 	}
 }
 
-@Composable
-private fun MyTargetButton(animatable: Animatable<Dp, AnimationVector1D>, targetValue: Dp, duration: Int) {
-	val scope = rememberCoroutineScope()
-	Button(onClick = {
-		scope.launch {
-			val animationResult = animatable.animateTo(targetValue, tween(duration, easing = LinearEasing)) {
-				Log.d(TAG, "velocity = $velocity")
-			}
-			Log.d(TAG, "end reason = ${animationResult.endReason}")
-		}
-	}) {
-		Text(text = targetValue.toString())
+val MyEasing: Easing = Easing { fraction ->
+	when {
+		fraction < 0.25 -> 0F
+		fraction < 0.5 -> 0.25F
+		fraction < 0.75 -> 0.5F
+		fraction < 1 -> 0.75F
+		else -> 1F
 	}
 }
+private fun createSpec(): AnimationSpec<Dp> =
+	tween(
+		durationMillis = 1000,
+		easing = MyEasing
+	)
